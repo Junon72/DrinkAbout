43551bcab924a00718c66109a9b8f162b6bb0556
Jussi Nousiainen <juno.athome@gmail.com>
2019-10-08 17:42:20 +0200

    changed the expect clause

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,6 @@
 
-function whatCanIDrink(age) {
+whatCanIDrink = function(age) {
+    if(age<0) {
     return "Sorry. I can’t tell what drink because that age is incorrect!";
-}
+    }
+};
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,7 @@
 describe("Drink About", function() {
 	describe("Age check", function() {
 		it("should have a value", function() {
-			expected(whatCanIDrink).toBe();
+			expected(whatCanIDrink(age)).toBe(true);
 		});
 	});
 });
