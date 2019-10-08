09600b31ddc9b8a4d6ce400bf8dffd32d42781dd
Jussi Nousiainen <juno.athome@gmail.com>
2019-10-08 18:33:33 +0200

    added rest of the suit

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,20 @@
 
 whatCanIDrink = function(age) {
-    age = -1;
     if(age<0) {
     return "Sorry. I can’t tell what drink because that age is incorrect!";
+    }
+    if(age<14) {
+    return "Drink Toddy";
+    }
+    if(age<18) {
+        return "Drink Coke";
+    }
+    if(age<21) {
+        return "Drink Beer";
+    }
+    if(age,130) {
+        return "Drink Whiskey";
+    } else {
+        return "Sorry,Sorry. I can’t tell what drink because that age is incorrect!";
     }
 };
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,19 @@
 describe("Drink About", function() {
 	describe("Age check", function() {
 		it("should have a value", function() {
-			expected(whatCanIDrink).toBe();
-		});
+			expect(whatCanIDrink(-1)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        it("should return Drink Toddy if age given is 13", function() {
+            expect(whatCanIDrink(13)).toBe("Drink Toddy");
+        });
+        it("should return Drink Coke if age given is 17", function() {
+            expect(whatCanIDrink(17)).toBe("Drink Coke");
+        });
+        it("should return Drink Beer if age given is 20", function() {
+            expect(whatCanIDrink(20)).toBe("Drink Beer");
+        });
+        it("should return Drink Toddy if age given is 129", function() {
+            expect(whatCanIDrink(129)).toBe("Drink Whiskey");
+        });
 	});
 });
